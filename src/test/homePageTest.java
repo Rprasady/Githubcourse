f16d0b31370e6d5c691bfae83bd1bb31c89af3a9
Raviprasad Yarapu <v-ryarapu@microsoft.com>
2022-01-01 16:57:30 +0530

    First Commit

---
+++ src/test/homePageTest.java
@@ -0,0 +1,8 @@
+package test;
+
+public class homePageTest {
+
+	public static void main(String a[]) {
+		System.out.println("Firdt project");
+	}
+}
---
+++ src/test/loginpagetest.java
@@ -0,0 +1,8 @@
+package test;
+
+public class loginpagetest {
+
+	public void test1(){
+		System.out.println("Login test page Test");
+	}
+}
